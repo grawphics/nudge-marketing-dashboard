689058149de5b3e388a2e6a97868449d327e13e6
Gaurav <rawatgauravofficial@gmail.com>
2024-03-01 03:30:17 +0530

    Added loading functionality to login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,44 +1,49 @@
+import { useState } from "react";
+
 export default function Login() {
-  
-    const handleSubmit = async (e) => {
-    
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    setIsLoading(true);
     const email = e.target.elements.email.value;
     const password = e.target.elements.password.value;
     const role = e.target.elements.role.value;
     console.log(`Email : ${email} Password:${password} Role: ${role}`);
 
-    const url = 'https://api.nudgenow.com/clients/login';
+    const url = "https://api.nudgenow.com/clients/login";
     const options = {
-        method : "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 
-            "email" : email ,
-            "password": password,
-            "role" : role })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        role: role,
+      }),
     };
-    
-    try{
-        const response = await fetch(url , options);
-        if(!response.ok) {
-            console.log("Error" , response.message)
-        }
-        const data = await response.json()
-        console.log(data)
-    } catch(err){
-        console.log(err)
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        console.log("Error", response.message);
+      }
+      const data = await response.json();
+      setIsLoading(false)
+      setIsLoggedIn(true)
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false)
+      setIsLoggedIn(false)
     }
-
-
-
-
   };
 
   return (
     <>
+    {!isLoggedIn && 
       <div className="w-full h-[100vh] flex justify-center items-center">
         <form
           onSubmit={handleSubmit}
@@ -51,10 +56,14 @@ export default function Login() {
             <option value="owner">Owner</option>
             <option value="co-owner">Co-owner</option>
           </select>
-          <button type="submit" className="w-24 rounded-sm bg-blue-600 text-white px-auto">Login</button>
+          <button
+            type="submit"
+            className="w-24 rounded-sm bg-blue-600 text-white px-auto"
+          >
+            {isLoading ? 'Wait ...' : 'Login'}
+          </button>
         </form>
-      </div>
-      
+      </div>}
     </>
   );
 }
